652e843b092f6d44d83a267d37c02d6ae6ca1c55
pera-kojot-genije <47058987+pera-kojot-genije@users.noreply.github.com>
2019-02-22 17:07:29 +0100

    Update ftui.js

--- ftui.js
+++ ftui.js
@@ -2,27 +2,45 @@ const inputFirstName = document.getElementById('firstname');
 const inputLastName = document.getElementById('lastname');
 const inputEmail = document.getElementById('email');
 const button = document.querySelector('.submit-button');
-const regEx = /\S+@\S+\.\S+/;
+const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const form = document.querySelector('.form');
+const progressIcon = document.getElementById('progressIcon') ;
 
 function checkInput() {
+  let retValue = false;
   if (inputFirstName.value.trim() !== "" && inputLastName.value.trim() !== "" && regEx.test(inputEmail.value)) {
     button.style.backgroundColor = '#f38221';
-    document.getElementById('ik0').src = 'bcheck.jpg';
+    progressIcon.src = 'bcheck.jpg';
     button.disabled = false;
+    retValue = true;
   } else {
-    button.style.backgroundColor = '#2196f3';
-    document.getElementById('ik0').src = 'b0.jpg';
-    button.disabled = true;
+    if (inputFirstName.value.trim() === "" && inputLastName.value.trim() === "" && inputEmail.value.trim() === "") {
+      button.style.backgroundColor = '#2196f3';
+      progressIcon.src = 'b0.jpg';
+      button.disabled = true;
+    }
+    if (inputFirstName.value.trim() !== "" && inputLastName.value.trim() === "" && inputEmail.value.trim() === "") {
+      progressIcon.src = 'b25.jpg';
+    }
+    if (inputFirstName.value.trim() !== "" && inputLastName.value.trim() !== "" && inputEmail.value.trim() === "") {
+      progressIcon.src = 'b50.jpg';
+    }
+    if (inputEmail.value.trim() !== "" && !regEx.test(inputEmail.value)) {
+      progressIcon.src = 'b75.jpg';
+      alert('E mail address is not correct');
+    }
   }
+  return retValue;
 }
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log(event);
-    console.log(inputFirstName.value);
-    console.log(inputLastName.value);
-    console.log(inputEmail.value);
-    console.log('Submitted');
-    
+    // Samo ako je forma dobro ispunjena
+    if (checkInput()) {
+      console.log(event);
+      console.log(inputFirstName.value);
+      console.log(inputLastName.value);
+      console.log(inputEmail.value);
+      console.log('Submitted');
+    }
 })
